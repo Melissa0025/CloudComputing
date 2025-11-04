19128395a56a297abd4804a72cdb1ea76ae023ed
Melissa0025 <melissadarocha25@gmail.com>
2025-11-04 23:17:29 +0530

    did changes to server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,6 +6,11 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
+app.get("/", (req, res) => {
+  res.send("Employee Directory Backend is running!");
+});
+
+
 // For Azure, use 0.0.0.0 instead of localhost
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`✅ Server running on port ${PORT}`);
