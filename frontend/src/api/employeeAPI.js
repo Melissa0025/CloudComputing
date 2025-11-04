66766aa9a477573be9dc8baa8bfdf8aac5030260
Melissa0025 <melissadarocha25@gmail.com>
2025-11-05 00:28:56 +0530

    commit 2

--- frontend/src/api/employeeAPI.js
+++ frontend/src/api/employeeAPI.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 //const API_BASE_URL = "http://localhost:5000/api/employees";
 
-const API_URL = "https://employee-directory-backend.azurewebsites.net/api";
+const API_BASE_URL = "https://employee-directory-backend.azurewebsites.net/api";
 
 
 // Get all employees with filters, search, pagination, and sorting
